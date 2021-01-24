73e036a581a3dc81cccfae8a35c79028863e848b
Alex-code89 <70084678+Alex-code89@users.noreply.github.com>
2021-01-24 12:09:26 +0800

    Added help button Get rid off commented lines

--- NYSEPro/UserPanel.xaml.cs
+++ NYSEPro/UserPanel.xaml.cs
@@ -49,23 +49,15 @@ namespace NYSEPro
 
         private void SearchBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            WebServicesSetting();
-            // serach by symbol
-            //HttpResponseMessage message = client.GetAsync("usersDT?symbol=" + textBox1.Text + "").Result;
-            //string userJson = message.Content.ReadAsStringAsync().Result;
-            //MessageBox.Show(userJson);
-            //var table = splitString(userJson);
-            //UserDataGrid.ItemsSource = table.DefaultView; 
+            //Load our web services base url
 
+            WebServicesSetting();
+          
             //Search by date
             DateTime fromDate = DateTime.Parse(FromTxt.Text);
             DateTime toDate = DateTime.Parse(ToTxt.Text);
             HttpResponseMessage message = client.GetAsync("UsersDateSearch?startDate=" + fromDate + "&endDate=" + toDate + "").Result;
             string userJson = message.Content.ReadAsStringAsync().Result;
-
-
-            MessageBox.Show(userJson);
-            //var table = JsonConvert.DeserializeObject<DataTable>(userJson);
             var table = splitString(userJson);
             UserDataGrid.ItemsSource = table.DefaultView; ;
 
@@ -74,155 +66,11 @@ namespace NYSEPro
         }
       
 
-        
-        
-        void SaveBtn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void HelpBtn_Click(object sender, RoutedEventArgs e)
         {
-            // ------------< btnApp_ScreenShot_as_File_Click() > ------------
-
-save_FrameworkElement_as_Screenshot_File(this);
-
-            //------------</ btnApp_ScreenShot_as_File_Click() >------------
-
-        }
-
-
-
-
-
-        private void save_FrameworkElement_as_Screenshot_File(FrameworkElement element)
-
-        {
-
-            //------------< save_FrameworkElement_as_Screenshot_File() >------------
-
-            //< init >
-
-            String filename = "‪C:\\Users\\Ellie\\Desktop\\test\\ellie-" + DateTime.Now.ToString("ddMMyyyy-hhmmss") + ".png";
-
-            //</ init >
-
-
-
-            //< get Screenshot of Element >
-
-            RenderTargetBitmap bmp = new RenderTargetBitmap((int)element.ActualWidth, (int)element.ActualHeight, 96, 96, PixelFormats.Pbgra32);
-
-            bmp.Render(element);
-
-            //</ get Screenshot of Element >
-
-
-
-            //< create Encoder >
-
-            PngBitmapEncoder encoder = new PngBitmapEncoder();
-
-            encoder.Frames.Add(BitmapFrame.Create(bmp));
-
-            //</ create Encoder >
-
-
-
-            //< save >
-
-            FileStream fs = new FileStream(filename, FileMode.Create);
-
-            encoder.Save(fs);
-
-            fs.Close();
-
-            //</ save >
-
-            //------------</ save_FrameworkElement_as_Screenshot_File() >-
-            //SaveFileDialog saveFileDialog = new SaveFileDialog();
-            //saveFileDialog.DefaultExt = ".txt";
-            //saveFileDialog.Filter = "Text documents (.txt)|*.txt";
-            //if (saveFileDialog.ShowDialog() == true)
-            //    File.WriteAllText(saveFileDialog.FileName, UserDataGrid.Items.SourceCollection.ToString());
-            //if (UserDataGrid.Items.Count > 0)
-            //{
-            //    SaveFileDialog sfd = new SaveFileDialog();
-            //    sfd.Filter = "PDF (*.pdf)|*.pdf";
-            //    sfd.FileName = "Output.pdf";
-            //    bool fileError = false;
-            //    Nullable<bool> result = sfd.ShowDialog();
-            //    //MessageBoxResult result = MessageBox.Show("Insert with Logo?", "Logo", MessageBoxButton.YesNoCancel);
-
-            //    if (result == true)
-            //    {
-            //        if (File.Exists(sfd.FileName))
-            //        {
-            //            try
-            //            {
-            //                File.Delete(sfd.FileName);
-            //            }
-            //            catch (IOException ex)
-            //            {
-            //                fileError = true;
-            //                MessageBox.Show("It wasn't possible to write the data to the disk." + ex.Message);
-            //            }
-            //        }
-            //        if (!fileError)
-            //        {
-            //            try
-            //            {
-            //                PdfPTable pdfTable = new PdfPTable(UserDataGrid.Columns.Count);
-            //                pdfTable.DefaultCell.Padding = 3;
-            //                pdfTable.WidthPercentage = 100;
-            //                pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
-
-            //                foreach (DataGridColumn column in UserDataGrid.Columns)
-            //                {
-            //                    PdfPCell cell = new PdfPCell(new Phrase(column.HeaderStringFormat));
-            //                    pdfTable.AddCell(cell);
-            //                }
-
-            //                foreach (DataGridRow row in UserDataGrid.Items)
-            //                {
-            //                    //foreach (DataGridCell cell in row.Selected)
-            //                    //{
-            //                    //    if (cell.Content == null)
-            //                    //    {
-            //                    //        pdfTable.AddCell("0");
-            //                    //    }
-            //                    //    else
-            //                    //    {
-            //                    //        pdfTable.AddCell(cell.Content.ToString());
-
-            //                    //    }
-            //                    //}
-            //                    int i = row.GetIndex();
-            //                    pdfTable.GetRow(i).ToString();
-            //                }
-
-            //                using (FileStream stream = new FileStream(sfd.FileName, FileMode.Create))
-            //                {
-            //                    Document pdfDoc = new Document(PageSize.A4, 10f, 20f, 20f, 10f);
-            //                    PdfWriter.GetInstance(pdfDoc, stream);
-            //                    pdfDoc.Open();
-            //                    pdfDoc.Add(pdfTable);
-            //                    pdfDoc.Close();
-            //                    stream.Close();
-            //                }
-
-            //                MessageBox.Show("Data Exported Successfully !!!", "Info");
-            //            }
-            //            catch (Exception ex)
-            //            {
-            //                MessageBox.Show("Error :" + ex.Message);
-            //            }
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    MessageBox.Show("No Record To Export !!!", "Info");
-            //}
-        }
-
-        private void SearchBtn_Click(object sender, RoutedEventArgs e)
-        {
+            MessageBox.Show(@"Date format for search is : yyyy/dd/mm
+You can get a pdf of your search result by clicking on the save button.
+For more enquries contact our IT team");
 
         }
     }
